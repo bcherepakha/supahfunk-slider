29e6fc3543f8a49fd6e4c755cb611e34b058d98f
Boris Cherepakha <bcherepakha@gmail.com>
2023-04-27 18:39:47 +0300

    initial

---
+++ slider.js
@@ -0,0 +1,27 @@
+const slider = {
+    items: Array.from(document.querySelectorAll('.carousel-item')),
+    currentIndex: 0,
+    clickByItem(index) {
+        this.currentIndex = index;
+        this.render();
+    },
+    render() {
+        this.items.forEach(function (item, index) {
+            item.style.setProperty('--progress', index - this.currentIndex);
+            item.style.setProperty('--active', +(index === this.currentIndex));
+
+            const opacity = 3*Math.abs(index - this.currentIndex) / (this.items.length - 1);
+
+            item.style.setProperty('--opacity', index === this.currentIndex ? 1 : opacity );
+        }, this);
+    },
+    init() {
+        this.items.forEach(function (item, index) {
+            item.addEventListener('click', this.clickByItem.bind(this, index));
+        }, this);
+
+        this.render();
+    }
+}
+
+slider.init();
